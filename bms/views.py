c6c130a8cc3aedf73c47ea54f6b114dce74b2852
liuxun931 <liuxun931@gmail.com>
2016-10-25 18:55:21 +0800

    first update url and view
    first update url and view

--- bms/views.py
+++ bms/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def main(request):
+	context = "you are looking at main page."
+	return render(request, 'bms/main.html', {'context': context})
